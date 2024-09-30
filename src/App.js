42e5e1081f96dd042106e0f64f3260206a208e6c
Khushi Singh <khushi.singh092001@gmail.com>
2024-09-30 13:22:23 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl bg-blue-500 font-bold underline">Hello World!</h1>
+      <h1 className="text-3xl bg-blue-500 font-bold underline"> Welcome to my Bus-Service website</h1>
     </div>
   );
 }
